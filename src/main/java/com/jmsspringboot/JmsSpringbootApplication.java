9b1f9c65f392581b47a352ac2d9eb1723b7599c8
Karolina <200626@student.pwr.edu.pl>
2022-11-08 10:51:02 +0100

    Initial commit

---
+++ src/main/java/com/jmsspringboot/JmsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jmsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jmsspringboot/JmsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
