8feb5e85a4948f3bee263caa4d1064f830c84e12
Karolina <200626@student.pwr.edu.pl>
2022-11-08 11:42:10 +0100

    Added Configuration for sending message with ActiveMQ and added Sender

---
+++ src/main/java/com/jmsspringboot/sender/HelloSender.java
@@ -0,0 +1,33 @@
+package com.jmsspringboot.sender;
+
+import com.jmsspringboot.config.JmsConfig;
+import com.jmsspringboot.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class HelloSender {
+
+    private final JmsTemplate jmsTemplate;
+
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage() {
+        System.out.println("I'm sending example message");
+
+        HelloWorldMessage message = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("Hello World")
+                .build();
+
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+
+        System.out.println("Message Sent!");
+
+    }
+
+}
